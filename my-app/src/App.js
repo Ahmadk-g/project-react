da2feb0e87c5127cfc7498f4572374b7a3cd783c
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-09-23 11:55:39 +0000

    Add Class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import FunctionalGreeting from "./components/FunctionalGreeting";
-import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+// import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
-      <FunctionalGreetingWithProps />
+      {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32"  /> */}
+      <StatefulGreetingWithPrevState greeting="I'm a stateful class component!" name="Mike" />
     </div>
   );
 }
