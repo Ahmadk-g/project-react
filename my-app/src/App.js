60aaf5c1d46697a0ff56649041d752b62e581668
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-10-10 12:50:11 +0000

    Alot of changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,36 @@ import './App.css';
 // import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+// import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+// import RenderingLists from './components/RenderingLists';
+import UncontrolledForm from './components/UncontrolledForm';
+import ControlledForm from './components/ControlledForm';
+import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
       {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32"  /> */}
-      <StatefulGreetingWithPrevState greeting="I'm a stateful class component!" name="Mike" />
+      {/* <StatefulGreetingWithPrevState greeting="I'm a stateful class component!" name="Mike" />
+      <EventsFunctional />
+      <EventsClass /> */}
+      {/* <MethodsAsPropsParent /> */}
+      {/* <RenderingLists /> */}
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledForm /> */}
+      {/* <SearchBar /> */}
+      {/* <ControlledFormHooks /> */}
+      {/* <UseStateWithArrays /> */}
+      <UseStateWithObjects />
+
     </div>
   );
 }
